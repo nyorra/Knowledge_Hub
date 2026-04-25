538be1640a712332126a8be0e35ecf6d379ff6a5
vlad <vlad1slav200508@yandex.ru>
2026-04-25 22:07:30 +0300

    feat: few CRUD endpoints

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from app.Settings import settings
 from app.schemas import AskRequest, AskResponse
+from app.services.storage import storage_service
 
 
 app = FastAPI()
@@ -20,6 +21,27 @@ app.add_middleware(
 async def get_ai_request(data: AskRequest):
     return AskResponse(status="success", answer=f"Получено: {data.text}")
 
-@app.post("/storage")
-async def get_sotrage_files():
-    pass
+
+@app.post("/storage/create")
+async def create_file(filename: str, content: str):
+    return storage_service.create_file(filename, content)
+
+@app.post("/storage/upload")
+async def upload_file(file: UploadFile = File(...)):
+    return storage_service.upload_file_from_pc(file.file, file.filename)
+
+@app.get("/storage/files")
+async def list_files():
+    return {"files": storage_service.get_all_files()}
+
+@app.get("/storage/content")
+async def file_content(filename: str):
+    return {"file content": storage_service.get_file_content(filename)}
+
+@app.put("/storage/edit")
+async def edit_file(filename: str, content: str):
+    return storage_service.edit_file(filename, content)
+
+@app.delete("/storage/delete")
+async def delete_file(filename: str):
+    return storage_service.delete_file(filename)
